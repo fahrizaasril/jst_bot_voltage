eabcc617f63f2079324ea91c8322961242ef5bdb
fahrizaasril <84886351+fahrizaasril@users.noreply.github.com>
2021-06-04 11:52:38 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,30 +2,32 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js'); // predict
-const cls_model = require('./sdk/cls_model.js'); // cls
+const model = require('./sdk/model.js');
+const cls_model = require('./sdk/cls_model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1659364972:AAFRXlLjVN-HcgQfce1WZiQNC6J5rJmf7OI'
 const bot = new TelegramBot(token, {polling: true});
 
-state = 0;
-// Main Menu Bot
+
+// bots
 bot.onText(/\/start/, (msg) => { 
+    console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
-    );
-    state = 0;
+        click  /predict`
+    );   
 });
 
-// input requires i and r
+
+
+state = 0;
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai i|v contohnya 9|9`
+        `input nilai i|v example 9|9`
     );   
     state = 1;
 });
@@ -33,31 +35,31 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message', (msg) => {
     if(state == 1){
         s = msg.text.split("|");
-        i = parseFloat(s[0])
-        r = parseFloat(s[1])
-        
+        i = s[0]
+        r = s[1]
         model.predict(
             [
-                i, // string to float
-                r
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
             ]
-        ).then((jres1)=>{
-            console.log(jres1);
+        ).then((jres)=>{
+            v = parseFloat(jres[0])
+            p = parseFloat(jres[1])
             
-            cls_model.classify([i, r, parseFloat(jres1[0]), parseFloat(jres1[1])]) .then((jres2)=>{
+            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), p, v]).then((jres2) =>{
                 bot.sendMessage(
-                        msg.chat.id,
-                        `nilai v yang di prediksi adalah ${v} volt`
+                    msg.chat.id,
+                    `Nilai v yang diprediksi adalah ${jres[0]} volt`
+                );   
+                bot.sendMessage(
+                    msg.chat.id,
+                    `Nilai p yang diprediksi adalah ${jres[1]} watt`
                 );
                 bot.sendMessage(
-                        msg.chat.id,
-                        `nilai p yang di prediksi adalah ${p} volt`
-                );
-                bot.sendMessage(
-                        msg.chat.id,
-                        `Klasifikasi Tegangan ${jres2}`
-                );
-            })
+                    msg.chat.id,
+                    `Klasifikasi tegangan ${jres2}`
+                );   
+            }) 
         })
     }else{
         state = 0
@@ -65,7 +67,7 @@ bot.on('message', (msg) => {
 })
 
 // routers
-r.get('/predict/:i/:r', function(req, res, next) {    
+r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.i), // string to float
@@ -77,24 +79,24 @@ r.get('/predict/:i/:r', function(req, res, next) {
 });
 
 // routers
-r.get('/classify/:i/:r', function(req, res, next) {    
-    model.predict(
+r.get('/classify/:i/:r', function(req, res, next) {
+      model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+                parseFloat(req.params.i), //string nofloat
+                parseFloat(req.params.r),
         ]
-     ).then((jres)=>{
-        cls_model.classify(
-            [
-                parseFloat(req.params.i), // string to float
+       ).then((jres)=>{
+           cls_model.classify(
+               [
+                parseFloat(req.params.i), //string nofloat
                 parseFloat(req.params.r),
                 parseFloat(jres[0]),
                 parseFloat(jres[1])
-            ]
-        ).then((jres)=>{
-            res.json(jres_)
-        })
-    })
+               ]
+               ).then((jres_)=>{
+                res.json(jres_)
+           })
+      })
 });
-
+        
 module.exports = r;
