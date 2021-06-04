2bc26e86319a4f25623ee25ce704392e931c872e
fahrizaasril <84886351+fahrizaasril@users.noreply.github.com>
2021-06-04 11:45:38 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -4,7 +4,7 @@ function normalized(data){ // i & r
     i = (data[0] - 12.585) / 6.813882
     r = (data[1] - 51.4795) / 29.151289
     v = (data[2] - 650.4795) / 552.6351
-    p = (data[3] - 10620.56) / 12152.78
+    p = (data[3] - 10620.56) / 12152.78   
     return [i, r, v, p]
 }
 
@@ -12,26 +12,32 @@ const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]).redu
 const argMax = argFact((min, el) => (el[0] > min[0] ? el : min))
 
 function ArgMax(res){
-    label = "NORMAL"
-    if(argMax(res) == 1){
+  label = "NORMAL"
+  cls_data = []
+    for(i=0; i<res.length; i++){
+        cls_data[i] = res[i]
+    }
+    console.log(cls_data, argMax(cls_data));
+    
+    if(argMax(cls_data) == 1){
         label = "OVER VOLTAGE"
-    }if(argMax(res) == 2){
+    }if(argMax(cls_data) == 0){
         label = "DROP VOLTAGE"
     }
-    return label
+  return label  
 }
 
 async function classify(data){
-    let in_dim = 4; // i r v p
+    let in_dim = 4; // i r v p 
     
     data = normalized(data);
-    shape = [1, in_dim];
+    shape = [1, in_dim]; 
 
     tf_data = tf.tensor2d(data, shape);
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/fahrizaasril/jst_bot_voltage/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/fahrizaasril/jst_bot_voltage/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
